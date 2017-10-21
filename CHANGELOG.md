34c10d12c7c971e889fdcd288a2cc4494f456f0d
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-10-21 03:07:31 +0000

    # SSKTS-651 ### Changed - 座席予約の供給情報インターフェースを調整。 - イベントステータスをエクスポート。

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 ### Added
 
 ### Changed
+- 座席予約の供給情報インターフェースを調整。
+- イベントステータスをエクスポート。
 
 ### Deprecated
 
--- src/factory/reservation/event.ts
+++ src/factory/reservation/event.ts
@@ -1,5 +1,5 @@
 /**
- * event reservation factory
+ * イベント予約ファクトリー
  * @namespace reservation.event
  */
 
@@ -11,8 +11,7 @@ import { IReservation, ReservationType } from '../reservation';
 import ArgumentError from '../../error/argument';
 
 import * as IndividualScreeningEventFactory from '../event/individualScreeningEvent';
-import { ISeatReservationOffer } from '../offer';
-import PriceCurrency from '../priceCurrency';
+import { IOfferWithDetails as ISeatReservationOffer } from '../offer/seatReservation';
 import ReservationStatusType from '../reservationStatusType';
 
 export interface IEventReservation<T extends IEvent> extends IReservation {
@@ -22,20 +21,26 @@ export interface IEventReservation<T extends IEvent> extends IReservation {
     reservationFor: T;
 }
 
+/**
+ * 座席仮予約からイベント予約データを作成する
+ * @export
+ * @function
+ * @memberof reservation.event
+ */
 export function createFromCOATmpReserve(params: {
     updTmpReserveSeatResult: COA.services.reserve.IUpdTmpReserveSeatResult;
     offers: ISeatReservationOffer[],
     individualScreeningEvent: IndividualScreeningEventFactory.IEvent
 }): IEventReservation<IndividualScreeningEventFactory.IEvent>[] {
-    const ticketInfos = params.offers.map((offer) => offer.ticketInfo);
-
     return params.updTmpReserveSeatResult.listTmpReserve.map((tmpReserve, index) => {
-        const selectedTicket = ticketInfos.find((ticket) => ticket.seatNum === tmpReserve.seatNum);
-        if (selectedTicket === undefined) {
-            throw new ArgumentError('offers', 'offers.ticketInfo not matched with tmpReserveSeatResult');
+        const requestedOffer = params.offers.find((offer) => {
+            return (offer.seatNumber === tmpReserve.seatNum && offer.seatSection === tmpReserve.seatSection);
+        });
+        if (requestedOffer === undefined) {
+            throw new ArgumentError('offers', '要求された供給情報と仮予約結果が一致しません。');
         }
 
-        // create ticket token (QR code string)
+        // チケットトークン(QRコード文字列)を作成
         const ticketToken = [
             params.individualScreeningEvent.coaInfo.theaterCode,
             params.individualScreeningEvent.coaInfo.dateJouei,
@@ -52,18 +57,18 @@ export function createFromCOATmpReserve(params: {
             additionalTicketText: '',
             modifiedTime: now,
             numSeats: 1,
-            price: selectedTicket.salePrice,
-            priceCurrency: PriceCurrency.JPY,
+            price: requestedOffer.price,
+            priceCurrency: requestedOffer.priceCurrency,
             reservationFor: params.individualScreeningEvent,
             reservationNumber: `${params.updTmpReserveSeatResult.tmpReserveNum}-${index.toString()}`,
             reservationStatus: ReservationStatusType.ReservationHold,
             reservedTicket: {
                 typeOf: 'Ticket',
-                coaTicketInfo: selectedTicket,
+                coaTicketInfo: requestedOffer.ticketInfo,
                 dateIssued: now,
                 issuedBy: params.individualScreeningEvent.superEvent.organizer,
-                totalPrice: selectedTicket.salePrice,
-                priceCurrency: PriceCurrency.JPY,
+                totalPrice: requestedOffer.price,
+                priceCurrency: requestedOffer.priceCurrency,
                 ticketedSeat: {
                     typeOf: 'Seat',
                     seatingType: '',
