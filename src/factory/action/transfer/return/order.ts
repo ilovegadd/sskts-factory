47159208d9c38ca9615d1344e4f6263b53599044
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-05-24 08:41:48 +0000

    # SSKTS-935 ### Added - Pecorino賞金返却アクション、Pecorino賞金返却タスクを追加。

--- src/factory/action/transfer/return/order.ts
+++ src/factory/action/transfer/return/order.ts
@@ -1,6 +1,3 @@
-/**
- * 注文返品アクションファクトリー
- */
 import * as ActionFactory from '../../../action';
 import { IOrder } from '../../../order';
 import PaymentMethodType from '../../../paymentMethodType';
@@ -10,23 +7,30 @@ import * as PecorinoAwardReturnActionFactory from './pecorinoAward';
 
 export type IAgent = ActionFactory.IParticipant;
 export type IRecipient = ActionFactory.IParticipant;
-
 /**
  * 返却対象は注文
  */
 export type IObject = IOrder;
-
 export type IResult = any;
-
 export interface IPotentialActions {
+    /**
+     * クレジットカード返金アクション
+     */
     refundCreditCard?: IRefundActionAttributes<PaymentMethodType.CreditCard>;
+    /**
+     * Pecorino返金アクション
+     */
     refundPecorino: IRefundActionAttributes<PaymentMethodType.Pecorino>[];
-    returnPecorinoAward?: PecorinoAwardReturnActionFactory.IAttributes;
+    /**
+     * Pecorino賞金返却アクション
+     */
+    returnPecorinoAward: PecorinoAwardReturnActionFactory.IAttributes[];
 }
-
 export interface IAttributes extends ReturnActionFactory.IAttributes<IObject, IResult> {
     recipient: IRecipient;
     potentialActions?: IPotentialActions;
 }
-
+/**
+ * 注文返品アクションインターフェース
+ */
 export type IAction = ReturnActionFactory.IAction<IAttributes>;
--- src/factory/action/transfer/return/pecorinoAward.ts
+++ src/factory/action/transfer/return/pecorinoAward.ts
@@ -1,26 +1,21 @@
 import * as ActionFactory from '../../../action';
-import * as GivePecorinoAwardActionFactory from '../give/pecorinoAward';
+import * as PecorinoAwardAuthorizeActionFactory from '../../authorize/award/pecorino';
 import * as ReturnActionFactory from '../return';
 
 export type IAgent = ActionFactory.IParticipant;
 export type IRecipient = ActionFactory.IParticipant;
-
 /**
- * 返却対象はPecorino賞金付与アクション
+ * 返却対象はPecorino賞金承認アクション
  */
-export type IObject = GivePecorinoAwardActionFactory.IAttributes;
-
+export type IObject = PecorinoAwardAuthorizeActionFactory.IAttributes;
 export type IResult = any;
-
 // tslint:disable-next-line:no-empty-interface
 export interface IPotentialActions {
 }
-
 export interface IAttributes extends ReturnActionFactory.IAttributes<IObject, IResult> {
     recipient: IRecipient;
     potentialActions?: IPotentialActions;
 }
-
 /**
  * Pecorino賞金返却アクションインターフェース
  */
