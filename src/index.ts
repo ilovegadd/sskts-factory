9cf1257c827aa3bbdf99528c2b7ae67f1649ad38
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-07-31 03:51:30 +0000

    # SSKTS-994 ### Changed - 口座タイプを追加して、Pecorinoに指定するように変更。

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * sskts-factory
  */
-import * as pecorinoFactory from '@motionpicture/pecorino-factory';
+import * as pecorinoFactory from '@pecorino/factory';
 
 import * as ActionFactory from './factory/action';
 import * as AuthorizeActionFactory from './factory/action/authorize';
@@ -30,6 +30,7 @@ import * as SendOrderActionFactory from './factory/action/transfer/send/order';
 import ActionStatusType from './factory/actionStatusType';
 import ActionType from './factory/actionType';
 
+import AccountType from './factory/accountType';
 import * as ClientEventFactory from './factory/clientEvent';
 import * as ClientUserFactory from './factory/clientUser';
 import * as EmailMessageFactory from './factory/creativeWork/message/email';
@@ -206,6 +207,7 @@ export namespace action {
     }
 }
 
+export import accountType = AccountType;
 export namespace paymentMethod {
     export namespace paymentCard {
         export import creditCard = CreditCardFactory;
